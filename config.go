080e1cc7d6419d90c63f2439dfa895d38bd978d5
Sharif Olorin <sio@tesser.org>
2014-06-25 15:20:20 +1000

    Remove old cruft we won't need
    Signed-off-by: Sharif Olorin <sio@tesser.org>

--- config.go
+++ config.go
@@ -4,12 +4,6 @@ package main
 // vaultaire-collector-nagios.
 type Config struct {
 	General struct {
-		// Writes to execute in parallel.
-		Parallelism int
-		// Writes to perform as one operation (where
-		// applicable).
-		BatchSize      int
-		StorageBackend string
 		LogFile        string
 		LogLevel       string
 		// Kill ourselves if we run longer than this
@@ -19,8 +13,5 @@ type Config struct {
 	Vaultaire struct {
 		// Namespace for the Marquise daemon.
 		Namespace string
-	}
-	File struct {
-		DataFrameFile string
 	}
 }
