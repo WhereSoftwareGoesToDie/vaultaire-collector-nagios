6572c4f68c375a2ffb7efe9085e02f001fbf5038
Sharif Olorin <sio@tesser.org>
2014-06-26 10:39:59 +1000

    More metric munging
    Signed-off-by: Sharif Olorin <sio@tesser.org>

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,22 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
 import Options.Applicative
 import qualified Data.ByteString as S
+import qualified Data.ByteString.Char8 as C
 import System.IO
 import System.IO.Error
 import Control.Exception
+import Data.Word
+import Data.Serialize
 
 import Data.Nagios.Perfdata
 import Marquise.Client
+
+(+++) :: S.ByteString -> S.ByteString -> S.ByteString
+(+++) = S.append
 
 data CollectorOptions = CollectorOptions {
     optNamespace :: String
@@ -31,6 +38,22 @@ collectorOptionParser =
         progDesc "Vaultaire collector for Nagios perfdata files" <>
         header "vaultaire-collector-nagios - writes datapoints from Nagios perfdata files to Vaultaire")
 
+
+getMetricId :: Perfdata -> String -> S.ByteString
+getMetricId datum metric = 
+    let host = perfdataHostname datum in
+    let service = S.unpack $ perfdataServiceDescription datum in
+    "host:" +++ (C.pack host) +++ ",metric:" +++ (C.pack metric) +++ ",service:" +++ (S.pack service) +++ ","
+
+unpackMetrics :: Perfdata -> [(Address,Word64)]
+unpackMetrics datum = 
+    zip (map (getAddress datum) (map fst (perfdataMetrics datum)))
+        (map (extractValueWord . snd) (perfdataMetrics datum))
+  where
+    getAddress p = hashIdentifier . (getMetricId p)
+    extractValueWord = either (const 0) id . extractValueWordEither
+    extractValueWordEither = decode . encode . (flip metricValueDefault 0.0)
+
 handleLines :: CollectorOptions -> IO ()
 handleLines op@CollectorOptions{..} = do
     line <- try S.getLine
@@ -40,7 +63,7 @@ handleLines op@CollectorOptions{..} = do
                 then return ()
                 else hPutStrLn stderr $ "Error reading perfdata: " ++ (show err)
         Right l -> do
-            S.putStrLn l
+            C.putStrLn l
             handleLines op
 
 main :: IO ()
