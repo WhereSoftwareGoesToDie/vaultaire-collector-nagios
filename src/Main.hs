e1979b0c4a4eb287d339f5f6367ce8e474191f53
Sharif Olorin <sio@tesser.org>
2014-07-21 09:40:02 +1000

    Minor version bump for output changes

--- src/Main.hs
+++ src/Main.hs
@@ -41,7 +41,7 @@ import Data.Nagios.Perfdata
 import Marquise.Client
 
 collectorVersion :: String
-collectorVersion = "2.0.0"
+collectorVersion = "2.0.1"
 
 (+.+) :: S.ByteString -> S.ByteString -> S.ByteString
 (+.+) = S.append
