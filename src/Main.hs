d0712b0fff5054901bdb92026668fec7df2067f2
Oswyn Brent <oswyn.brent@anchor.com.au>
2014-07-29 11:23:55 +1000

    Further refactoring

--- src/Main.hs
+++ src/Main.hs
@@ -7,7 +7,6 @@
 -- redistribute it and/or modify it under the terms of the BSD license.
 
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TupleSections #-}
 
 module Main where
@@ -15,21 +14,17 @@ module Main where
 import Cache
 import Options
 import State
+import Util
 
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as S
-import qualified Data.ByteString.Char8 as C
 import System.IO
 import System.IO.Error
 import Control.Exception
 import Control.Monad
 import Control.Monad.Reader
 import Data.Word
-import qualified Data.HashMap.Strict as HashMap(fromList)
-import qualified Data.Text as T
-import Data.Text (Text)
-import Data.Bifunctor (second,bimap)
-import Data.Binary.IEEE754 (doubleToWord)
+import Data.Bifunctor (second)
 import Data.Maybe
 import Data.Either (partitionEithers)
 import Data.Set hiding (map,partition)
@@ -43,68 +38,12 @@ import Marquise.Client
 collectorVersion :: String
 collectorVersion = "2.1.2"
 
-(+.+) :: S.ByteString -> S.ByteString -> S.ByteString
-(+.+) = S.append
-
 runCollector :: CollectorOptions -> CollectorMonad a -> IO a
 runCollector op@CollectorOptions{..} (CollectorMonad act) = do
     maybePut optDebug $ "Collector version " ++ collectorVersion ++ " starting."
     files <- createSpoolFiles optNamespace
     initialHashes <- getInitialCache optCacheFile (maybePut optDebug) 
     runReaderT act $ CollectorState op files initialHashes optCacheFile
-
--- | Returns the Vaultaire SourceDict for the supplied metric in datum,
--- or an error if the relevant values have invalid characters (',' or
--- ':'). 
-getSourceDict :: Perfdata -> String -> UOM -> Either String SourceDict
-getSourceDict datum metric uom = 
-    makeSourceDict . HashMap.fromList $ buildList datum metric uom
-
--- | Builds an association list for conversion into a SourceDict    
-buildList :: Perfdata -> String -> UOM -> [(Text, Text)] 
-buildList datum metric uom
-    | (uom == Counter) = convert $ ("_counter", "1"):baseList
-    | otherwise        = convert baseList
-  where
-    -- host, metric and service are collectively the primary key for
-    -- this metric. As the nagios-perfdata package currently treats
-    -- all values as floats, we also specify this as metadata for
-    -- the presentation layer.
-    host = perfdataHostname datum
-    service = C.unpack $ perfdataServiceDescription datum
-    baseList = zip ["host", "metric", "service", "_float", "_uom"] [host, metric, service, "1", show uom]
-    convert = map $ bimap T.pack fmtTag
-    
-fmtTag :: String -> Text
-fmtTag = T.pack . map ensureValid
-
-ensureValid :: Char -> Char
-ensureValid ',' = '-'
-ensureValid ':' = '-'
-ensureValid x = x
-
--- | Returns the unique identifier for the named metric in the supplied
--- perfdatum. This is used to calculate the address. 
-getMetricId :: Perfdata -> String -> S.ByteString
-getMetricId datum metric = 
-    let host = perfdataHostname datum in
-    let service = S.unpack $ perfdataServiceDescription datum in
-    "host:" +.+ C.pack host +.+ ",metric:" +.+ C.pack metric +.+ ",service:" +.+ S.pack service +.+ ","
-
--- | Returns the Vaultaire address for the specified metric. 
-getAddress :: Perfdata -> String -> Address
-getAddress p = hashIdentifier . getMetricId p
-
--- | Given a Perfdata object, extract each metric into a list of
--- (address,value) tuples.
-unpackMetrics :: Perfdata -> [(Address, Either String Word64)]
-unpackMetrics datum = 
-    map (bimap (getAddress datum) extractValueWord) (perfdataMetrics datum)
-  where
-    extractValueWord :: Metric -> Either String Word64
-    extractValueWord m = if unknownMetricValue m then
-        Left "unknown metric value" else
-        Right . doubleToWord $ metricValueDefault m 0.0
 
 -- | Queue updates to the metadata associated with each metric in the
 -- supplied perfdatum. Does not queue redundant updates
