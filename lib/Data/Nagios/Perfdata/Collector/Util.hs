684fae5ad23889d59dc52ababaf2db62bd5bcc45
Barney Desmond <barney.desmond@anchor.net.au>
2014-08-08 14:48:39 +1000

    Clean up code, remove evil Maybes and not-quite-filters

--- lib/Data/Nagios/Perfdata/Collector/Util.hs
+++ lib/Data/Nagios/Perfdata/Collector/Util.hs
@@ -10,7 +10,6 @@ import qualified Data.HashMap.Strict as HashMap(fromList)
 import qualified Data.Text as T
 import Data.Text (Text)
 import Data.Word
-import Data.Maybe
 
 import Data.Nagios.Perfdata
 import Data.Nagios.Perfdata.Metric
@@ -32,27 +31,26 @@ getSourceDict =
     makeSourceDict . HashMap.fromList
 
 
-type MetricThresholds = (Threshold,Threshold,Threshold,Threshold)
+type MetricThresholds = (Threshold, Threshold, Threshold, Threshold)
 
-perfdatumToThresholds :: Perfdata -> [ (String, MetricThresholds) ]
-perfdatumToThresholds p =
+perfdatumToThresholds :: Perfdata -> [(String, MetricThresholds)]
+perfdatumToThresholds perfdata =
     map (second metricThresholds) metricList
   where
-    metricList         = perfdataMetrics p -- MetricList == [(String,Metric)]
+    metricList         = perfdataMetrics perfdata
     metricThresholds m = (warnValue m, critValue m, minValue m, maxValue m)
 
-thresholdsToList :: (String, MetricThresholds) -> [ (String, String) ]
-thresholdsToList (_, (warn,crit,minThreshold,maxThreshold)) =
-    let maybeNagiosKeys = zip nagiosPerfdataKeys $ map maybeShow [warn,crit,minThreshold,maxThreshold] in
-    [ (x,fromJust y) | (x,y) <- maybeNagiosKeys, isJust y ]
+thresholdsToList :: MetricThresholds -> [(String, String)]
+thresholdsToList (warn, crit, minThreshold, maxThreshold) =
+    foldl filterOutNoThreshold [] $ zip nagiosPerfdataKeys [warn, crit, minThreshold, maxThreshold]
   where
+    filterOutNoThreshold acc (_, NoThreshold) = acc
+    filterOutNoThreshold acc (k, DoubleThreshold v) = (k, show v):acc
     nagiosPerfdataKeys = [ "_nagios_warn", "_nagios_crit", "_nagios_min", "_nagios_max" ]
-    maybeShow (DoubleThreshold v) = Just $ show v
-    maybeShow NoThreshold         = Nothing
 
 -- | Builds an association list for conversion into a SourceDict
 buildList :: Perfdata -> String -> UOM -> Bool -> [(Text, Text)]
-buildList datum metricName uom normalise = convert $ concat [baseList, counter, unit, nagiosPerfdata]
+buildList datum metricName uom normalise = convert $ concat [baseList, counter, unit, nagiosThresholds]
   where
     counter
         | uom == Counter = [("_counter", "1")]
@@ -60,8 +58,9 @@ buildList datum metricName uom normalise = convert $ concat [baseList, counter, 
     unit
         | normalise = [("_unit", "1")]
         | otherwise = []
-    nagiosPerfdata = concatMap thresholdsToList interestingThresholds
-    interestingThresholds = [ x | x <- perfdatumToThresholds datum, fst x == metricName ]
+    isInteresting = (== metricName) . fst
+    interestingThresholds = map snd $ filter isInteresting $ perfdatumToThresholds datum
+    nagiosThresholds = concatMap thresholdsToList interestingThresholds
     -- host, metricName and service are collectively the primary key for
     -- this metric. As the nagios-perfdata package currently treats
     -- all values as floats, we also specify this as metadata for
